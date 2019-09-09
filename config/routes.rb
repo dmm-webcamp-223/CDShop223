2108e5851e40ead569290a4a0b10d711ae144278
Kenchi247 <inmf78bk247@gmail.com>
2019-09-09 11:31:45 +0000

    add adminpackage

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,15 @@
 Rails.application.routes.draw do
-  
+  devise_for :admins
+
+  devise_for :users
+
   resources :carts, only: [:show]
   resources :cart_items, only: [:create, :update, :destroy]
+  resources :packages
 
   
   root 'packages#index'
-  devise_for :admins
 
-  devise_for :users do
-      resources :carts, only: [:show] do
-        resources :cart_items, only: [:create, :update, :destroy] 
-        get 'purchase_page' => 'cart_items#purchase_page'
-        get 'purchase_check' => 'cart_items#purchase_check'
-        get 'purchase_comfirmation' => 'cart_items#purchase_comfirmation'
-      end
-  end
-  
-        
-    
 
   	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
