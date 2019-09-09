4c8125456182e6325356850a24d795274ba18be2
shotatsuno <number9973x@gmail.com>
2019-09-09 11:29:58 +0000

    [Add routes]

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,32 @@
 Rails.application.routes.draw do
   
+      devise_for :admins
+
+      devise_for :users
+
+  
   resources :carts, only: [:show]
   resources :cart_items, only: [:create, :update, :destroy]
+  
+
+  
+  resources :users, only: [:index, :edit, :update, :show, :destroy] do
+      resources :carts, only: [:show] do
+         resources :cart_items, only: [:create, :update, :destroy] 
+         get 'purchase_page' => 'cart_items#purchase_page'
+         get 'purchase_check' => 'cart_items#purchase_check'
+         get 'purchase_confirmation' => 'cart_items#purchase_confirmation'
+      end
+  end
+    
+  
 
   
   root 'packages#index'
-  devise_for :admins
+  
 
-  devise_for :users do
-      resources :carts, only: [:show] do
-        resources :cart_items, only: [:create, :update, :destroy] 
-        get 'purchase_page' => 'cart_items#purchase_page'
-        get 'purchase_check' => 'cart_items#purchase_check'
-        get 'purchase_comfirmation' => 'cart_items#purchase_comfirmation'
-      end
-  end
+  
+  
   
         
     
