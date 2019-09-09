2c1d2ca0217c7f2507ebb820599840cc1878f55f
Yusuke-Miyajima <yuten7.09.01@gmail.com>
2019-09-09 06:11:34 +0000

    add users songs cart_items controller

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,19 @@
+class CartItemsController < ApplicationController
+  def create
+  end
+
+  def purchase_page
+  end
+
+  def purchase_check
+  end
+
+  def purchase_comfamation
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
