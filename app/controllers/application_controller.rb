af319855798d0c0a2daf3c684edbbcc072d48d23
Yusuke-Miyajima <yuten7.09.01@gmail.com>
2019-09-11 08:29:25 +0000

    fixed devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
+	protect_from_forgery with: :exception
+
+	helper_method :current_cart
+
+	def current_cart
+		if session[:cart_id]
+			@cart = Cart.find(session[:cart_id])
+		else
+			@cart = Cart.configure_permitted_parameterssession[:cart_id] = @cart.cart_id
+		end
+	end
 
   protected
     def configure_permitted_parameters
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :cart
+  has_many :recept_logs, dependent: :destroy
   acts_as_paranoid
 
   composed_of :fullname,
