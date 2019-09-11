72e64046706f714290e9ca2f357d8293c399b955
Kenchi247 <inmf78bk247@gmail.com>
2019-09-11 11:08:31 +0000

    911

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -5,25 +5,6 @@ class PackagesController < ApplicationController
 
   def show
   	@package = Package.find(params[:id])
-<<<<<<< HEAD
-  end
-  
-
-
-
-=======
     @discs = Disc.all.includes(:songs)
-  # @artists = Artist.all.includes(:songs)
   end
-      private
-    def package_params
-        params.require(:package).permit(:title, :disc_image, :price, 
-          discs_attributes: [:id, :song, :_destroy, 
-            songs_attributes: [:id, :disc_number, :_destroy]])
-    end
->>>>>>> 9e7bf532b10490fdd202666021d3fb5f802e1751
 end
-
-
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,44 @@
 Rails.application.routes.draw do
 
-  
+
       devise_for :admins
 
       devise_for :users
 
-  
-  resources :carts, only: [:show]
   resources :cart_items, only: [:create, :update, :destroy]
   resources :packages, only: [:index, :show]
   resources :admin_packages
 
-  
+
   resources :artists, only: [:new, :create, :destroy]
   resources :labels, only: [:new, :create, :destroy]
   resources :genres, only: [:new, :create, :destroy]
-  
- 
-  
-  
 
-  
+
+
+
+
+
   resources :users, only: [:index, :edit, :update, :show, :destroy] do
       resources :carts, only: [:show] do
-         resources :cart_items, only: [:create, :update, :destroy] 
+         resources :cart_items, only: [:create, :update, :destroy]
          get 'purchase_page' => 'cart_items#purchase_page'
          get 'purchase_check' => 'cart_items#purchase_check'
          get 'purchase_confirmation' => 'cart_items#purchase_confirmation'
       end
   end
-    
-  
 
-  
+
+
+
   root 'packages#index'
-  
 
-  
-  
-  
-        
-    
+
+
+
+
+
+
 
 
   	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
