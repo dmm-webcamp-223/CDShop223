a9bd482b7165386b206d1cc23fb62a14393d0f59
Kenchi247 <inmf78bk247@gmail.com>
2019-09-19 10:03:16 +0000

    919

--- app/models/recept_log.rb
+++ app/models/recept_log.rb
@@ -2,5 +2,5 @@ class ReceptLog < ApplicationRecord
 	belongs_to :user
 	has_many :purchase_data_logs, dependent: :destroy
 	validates :postal_code, :delivering_adddress, presence: true, on: :create
-	validates :total, presence: true, numericality: {only_integer: true, greater_than: 0}, on: :update
+	# validates :total, presence: true, numericality: {only_integer: true, greater_than: 0}, on: :update
 end
